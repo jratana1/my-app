cb07050da74450ea79daab550cbee54d382e9a47
John Ratana <jratana1@gmail.com>
2020-11-13 13:09:36 -0500

    new image

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>John</Text>
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
+      <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +20,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  }, 
 });
